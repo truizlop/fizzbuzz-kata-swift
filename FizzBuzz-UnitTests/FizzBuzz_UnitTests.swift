f870f692e0f408271686f95dfc7a4d5621ecb1ad
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-04-24 11:59:01 +0200

    Initial commit

---
+++ FizzBuzz-UnitTests/FizzBuzz_UnitTests.swift
@@ -0,0 +1,35 @@
+//
+//  FizzBuzz_UnitTests.swift
+//  FizzBuzz-UnitTests
+//
+//  Created by Tomás Ruiz López on 24/4/16.
+//  Copyright © 2016 Tomás Ruiz López. All rights reserved.
+//
+
+import XCTest
+
+class FizzBuzz_UnitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+use_frameworks!
+
+
+def testing_pods
+    pod 'Quick'
+    pod 'Nimble'
+end
+
+target 'FizzBuzz-Kata-Swift' do
+
+end
+
+target 'FizzBuzz-UnitTests' do
+    testing_pods
+end
+
