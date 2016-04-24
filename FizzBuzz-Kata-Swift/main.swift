ce52df55b1580fb2de2795f6f56f2238342a0cb0
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-04-24 11:54:04 +0200

    Initial Commit

---
+++ FizzBuzz-Kata-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FizzBuzz-Kata-Swift
+//
+//  Created by Tomás Ruiz López on 24/4/16.
+//  Copyright © 2016 Tomás Ruiz López. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
